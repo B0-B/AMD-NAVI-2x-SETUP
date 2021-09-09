68c570382bac81147b7567773b7bb7279baa7466
b1 <alch3mist94@protonmail.com>
2021-09-09 19:25:01 +0200

    fix in pull

--- pull.sh
+++ pull.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -O - https://b0-b.github.io/AMD-NAVI-1x-SETUP/setup.sh >> $HOME/setup.sh
-wget -O - https://b0-b.github.io/AMD-NAVI-1x-SETUP/uninstall.sh >> $HOME/uninstall.sh
+wget -O - https://b0-b.github.io/AMD-NAVI-2x-SETUP/setup.sh >> $HOME/setup.sh
+wget -O - https://b0-b.github.io/AMD-NAVI-2x-SETUP/uninstall.sh >> $HOME/uninstall.sh
