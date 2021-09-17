5e28c3f27edae2914dc1a354d8c8438ba96bd878
b1 <alch3mist94@protonmail.com>
2021-09-17 12:58:37 +0200

    fix uninstall

--- uninstall.sh
+++ uninstall.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt remove clinfo
+sudo apt remove clinfo -y
 $HOME/driver/amdgpu-pro-21.30-1290604-ubuntu-20.04/amdgpu-install --uninstall -y
 rm -r $HOME/driver $HOME/miner
 rm $HOME/setup.sh $HOME/uninstall.sh
